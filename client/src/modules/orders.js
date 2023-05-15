242b58f7506b34348e048cdebc2ffd151f197c20
jan@huggenberg.ch <jan@huggenberg.ch>
2023-05-15 16:41:17 +0200

    improved order form

--- client/src/modules/orders.js
+++ client/src/modules/orders.js
@@ -69,7 +69,7 @@ function onPrepareNewItem() {
   const form = document.createElement('form');
   form.action = `${process.env.SERVER}/api/order`;
   form.method = 'POST';
-  form.classList.add('new-item-form');
+  form.classList.add('new-item-form', 'form');
   form.addEventListener('submit', onCreateItem);
   form.innerHTML = `<section class="list-module__details__controls">
         <input type="submit" class="button-small" value="Create"/>
@@ -95,104 +95,221 @@ function onPrepareNewItem() {
     <div class="form__input-group">
       <label>
         Price
-        <input name="pricePerOrder" type="number" min="0.00" max="10000.00" step="0.1" placeholder="100" required />
-        <span class="unit">CHF</span>
+        <div class="form__input-unit">
+          <input name="pricePerOrder" type="number" min="0.00" max="10000.00" step="0.1" placeholder="100" required />
+          <span class="unit">CHF</span>
+        </div>
       </label>
     </div>
 
     <div class="form__input-group">
       <label>
         Description
-        <textarea name="description" placeholder="Write something here"></textarea>
+        <textarea rows="1" name="description" placeholder="Write something here"></textarea>
       </label>
     </div>
-    
-    <div class="form__input-group">
-    <label>
-      Repeat
-      <select name="interval" required>
-          <option value="none">None</option>
-          <option value="7">Every week</option>
-          <option value="14">Every 2 weeks</option>
-          <option value="28">Every month</option>
-          <option disabled>──────────</option>
-          <option value="custom">Custom</option>
-      </select>
-    </label>
-  </div>
 
-  <div class="form__input-group">
-    <label>
-      End Date
-      <input type="date" name="endDate" required />
-    </label>
-  </div>`;
+    <fieldset>
+      <legend>Delivery</legend>
+      <div class="form__input-group">
+        <label>
+          Type
+          <select name="delivery" required>
+              <option value="pickup">Pick-Up</option>
+              <option value="deliver">Deliver</option>
+          </select>
+        </label>
+      </div>
+    </fieldset>
+    
+    <fieldset>
+      <legend>Subscription</legend>
+      <div class="form__input-group">
+        <label>
+          Repeat
+          <select name="interval" required>
+              <option value="none">None</option>
+              <option value="7">Every week</option>
+              <option value="14">Every 2 weeks</option>
+              <option value="28">Every month</option>
+              <option disabled>──────────</option>
+              <option value="custom">Custom</option>
+          </select>
+        </label>
+      </div>
+    </fieldset>`;
+
+  listSection.replaceChildren(form);
 
   // handle on discard button
   const discardBtnEl = form.querySelector('.discard-btn');
   discardBtnEl.addEventListener('click', () => document.querySelector('#list-module__details').replaceChildren());
 
-  // handle on repeat selection
+  // add repeat select eventlistener
   const intervalEl = form.querySelector('select[name="interval"]');
   intervalEl.addEventListener('input', onSelectRepeat);
 
+  // add repeat select eventlistener
+  const deliveryEl = form.querySelector('select[name="delivery"]');
+  deliveryEl.addEventListener('input', onSelectDelivery);
+
+  // add autoresize behaviour
+  const textarea = form.querySelector('textarea[name="description"]');
+  textarea.addEventListener('input', onTextareaInput);
+
+  // handle on textarea input
+  // auto resize textarea
+  function onTextareaInput(event) {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+  }
+
+  // handle on delivery selection
+  function onSelectDelivery(event) {
+    const value = event.target.value;
+    const deliveryContainer = event.target.closest('.form__input-group');
+
+    if (value === 'pickup') {
+      // remove frequency input
+      removeInputGroupOf('textarea[name="deliveryAddress"]');
+    } else if (value === 'deliver') {
+      // add delivery address container
+      let deliveryAddressEl = document.querySelector('textarea[name="deliveryAddress"]');
+      if (!deliveryAddressEl) {
+        const deliveryAddressContainer = document.createElement('div');
+        deliveryAddressContainer.classList.add('form__input-group');
+        deliveryAddressContainer.innerHTML = `
+        <label>
+          Address
+          <textarea name="deliveryAddress" rows="2" placeholder="Hauserstrasse 128\n8400 Winterthur"></textarea>
+        </label>`;
+
+        deliveryContainer.after(deliveryAddressContainer);
+
+        // add autoresize behaviour
+        const textarea = form.querySelector('textarea[name="deliveryAddress"]');
+        textarea.addEventListener('input', onTextareaInput);
+      }
+    }
+  }
+
+  // handle on repeat selection
   function onSelectRepeat(event) {
     const value = event.target.value;
     const repeatContainer = event.target.closest('.form__input-group');
-    let endTypeEl = form.querySelector('select[name="endType"]');
 
-    if (value !== 'none' && !endTypeEl) {
-      const endTypeContainer = document.createElement('div');
-      endTypeContainer.classList.add('form__input-group');
-      endTypeContainer.innerHTML = `
-        <label>
-          End Repeat
-          <select name="endType" required>
-              <option value="never">Never</option>
-              <option value="after">After</option>
-              <option value="date">On Date</option>
-          </select>
-        </label>`;
+    // handle end repeat type
+    if (value === 'none') {
+      // remove end type
+      removeInputGroupOf('select[name="endType"]');
+
+      // remove frequency input
+      removeInputGroupOf('input[name="frequency"]');
+
+      // remove end date input
+      removeInputGroupOf('input[name="endDate"]');
+    } else {
+      // add end type
+      let endTypeEl = form.querySelector('select[name="endType"]');
+      if (!endTypeEl) {
+        const endTypeContainer = document.createElement('div');
+        endTypeContainer.classList.add('form__input-group');
+        endTypeContainer.innerHTML = `
+          <label>
+            End Repeat
+            <select name="endType" required>
+                <option value="never">Never</option>
+                <option value="after">After</option>
+                <option value="date">On Date</option>
+            </select>
+          </label>`;
 
         repeatContainer.after(endTypeContainer);
-
-      endTypeEl = endTypeContainer.querySelector('select[name="endType"]');
-      endTypeEl.addEventListener('input', onSelectRepeatEnd);
-
-      // display custom days field
-      if (value === "custom") {
-        console.log("every x days");
+        endTypeEl = endTypeContainer.querySelector('select[name="endType"]');
+        endTypeEl.addEventListener('input', onSelectRepeatEnd);
       }
-    } else if (value === 'none' && endTypeEl) {
-      endTypeContainer = endTypeEl.closest('.form__input-group');
-      endTypeContainer.remove();
+    }
+
+    // handle custom interval
+    if (value === 'custom') {
+      // add custom interval
+      let customIntervalEl = form.querySelector('input[name="customInterval"]');
+      if (!customIntervalEl) {
+        const customIntervalContainer = document.createElement('div');
+        customIntervalContainer.classList.add('form__input-group');
+        customIntervalContainer.innerHTML = `
+          <label>
+          Repeat every
+          <div class="form__input-unit">
+            <input name="customInterval" type="number" min="0" max="10000" step="1" placeholder="21" required />
+            <span class="unit">days</span>
+          </div>
+        </label>`;
+
+        repeatContainer.after(customIntervalContainer);
+      }
+    } else {
+      // remove custom interval
+      removeInputGroupOf('input[name="customInterval"]');
     }
   }
 
+  // handle on repeat end selection
   function onSelectRepeatEnd(event) {
     const value = event.target.value;
     const repeatEndContainer = event.target.closest('.form__input-group');
-    let frequencyEl = document.querySelector('select[name="frequency"]');
 
-    console.log(frequencyEl);
-
-    if (value === 'after' && !frequencyEl) {
-      const frequencyContainer = document.createElement('div');
-      frequencyContainer.classList.add('form__input-group');
-      frequencyContainer.innerHTML = `
-        <label>
-          How many times?
-          <input type="number" name="frequency" min="2" max="10000" step="1" placeholder="5" required />
-        </label>`;
+    // handle frequency input
+    if (value === 'after') {
+      // add frequency container
+      let frequencyEl = document.querySelector('input[name="frequency"]');
+      if (!frequencyEl) {
+        const frequencyContainer = document.createElement('div');
+        frequencyContainer.classList.add('form__input-group');
+        frequencyContainer.innerHTML = `
+          <label>
+            End after
+            <div class="form__input-unit">
+              <input type="number" name="frequency" min="2" max="10000" step="1" placeholder="5" required />
+              <span class="unit">times</span>
+            </div>
+          </label>`;
 
         repeatEndContainer.after(frequencyContainer);
-    } else if (value === 'never' && frequencyEl) {
-      frequencyContainer = frequencyEl.closest('.form__input-group');
-      frequencyContainer.remove();
+      }
+    } else {
+      // remove frequency input
+      removeInputGroupOf('input[name="frequency"]');
+    }
+
+    // handle end date input
+    if (value === 'date') {
+      let endDateEl = document.querySelector('input[name="endDate"]');
+      if (!endDateEl) {
+        const endDateEl = document.createElement('div');
+        endDateEl.classList.add('form__input-group');
+        endDateEl.innerHTML = `
+          <label>
+            End On
+            <input type="date" name="endDate" required />
+          </label>`;
+
+        repeatEndContainer.after(endDateEl);
+      }
+    } else {
+      // remove end date input
+      removeInputGroupOf('input[name="endDate"]');
     }
   }
 
-  listSection.replaceChildren(form);
+  // remove input group
+  function removeInputGroupOf(selector) {
+    let el = document.querySelector(selector);
+    if (el) {
+      container = el.closest('.form__input-group');
+      container.remove();
+    }
+  }
 }
 
 function onCreateItem() {}
@@ -207,7 +324,6 @@ function onSearchItem(event) {
  * @param {Object} list data list
  */
 function getListEl(list) {
-  console.log(list);
   const listEl = document.createElement('ul');
 
   if (list.length) {
